aa2a0db86ea4dd13ecdcf240c4cf71f3199af63e
PradheePKumar_R <pradheepkumarcit@outlook.com>
2020-06-27 19:32:54 +0530

    Updates

---
+++ precache-manifest.ccf85a532510b479ab43b51176a62305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e61e891412193da0e1621e82520b9dac",
+    "url": "/iampk94/rick-marty/index.html"
+  },
+  {
+    "revision": "3d7098c6c671093d6ba5",
+    "url": "/iampk94/rick-marty/static/css/2.0e1a4c73.chunk.css"
+  },
+  {
+    "revision": "486d7a439311966ab1db",
+    "url": "/iampk94/rick-marty/static/css/main.d2dba197.chunk.css"
+  },
+  {
+    "revision": "3d7098c6c671093d6ba5",
+    "url": "/iampk94/rick-marty/static/js/2.f6dee28c.chunk.js"
+  },
+  {
+    "revision": "486d7a439311966ab1db",
+    "url": "/iampk94/rick-marty/static/js/main.f88351ae.chunk.js"
+  },
+  {
+    "revision": "29bc45bbe2659307765b",
+    "url": "/iampk94/rick-marty/static/js/runtime~main.ba2aa2f0.js"
+  }
+]);
